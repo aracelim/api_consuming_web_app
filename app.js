d563f5c014727c039a85f6a50f41ed28c7c88092
Sam Weldemariam <sweldemariam@gmail.com>
2016-08-25 17:21:57 -0700

    (feat): get users functionality and users page

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 (function ($) {
   var baseUrl = "https://superqa-api-test.herokuapp.com";
 
+  $(".button-collapse").sideNav();
   $(".register").click(registerUser);
   $(".login").click(loginUser);
+  $(".logout").click(logoutUser);
+  $( document ).ready(populateUsers);
 
+  function populateUsers() {
+    if ($(".striped tbody")[0]) {
+      return getUsers();
+    }
+  }
+
+  function logoutUser() {
+    $(location).attr('href', window.location.pathname.replace("/users", "/login"))
+  }
   function loginUser() {
     var url = baseUrl + "/login";
     var method = "POST";
@@ -16,12 +28,35 @@
       url: url,
       data: payload
     }).done(function ( data ) {
-      console.log(data)
-      $.cookie("session-id", data.token);
       $(location).attr('href', window.location.pathname.replace("/login", "/users"))
     }).fail(function ( err ) {
       $(".data-error").html(err.responseJSON.message);
     });
+  }
+
+  function getUsers() {
+    var url = baseUrl + "/users";
+    $.ajax({
+      url: url
+    }).done(function ( data ) {
+      console.log(data.users);
+      var users = data.users;
+      var $tbody = $(".striped tbody");
+
+      users.forEach(function (user) {
+        var $email = $("<td>" + user.email + "</td>");
+        var $createdAt = $("<td>" + user.createdAt + "</td>");
+        var $role = $("<td>" + user.role + "</td>");
+        var $tr = $("<tr>")
+
+        $tr.append($email, $createdAt, $role);
+
+        $tbody.append($tr);
+      })
+    }).fail(function ( err ) {
+      $(".data-error").html(err.responseJSON.message);
+    });
+
   }
 
   function registerUser() {
